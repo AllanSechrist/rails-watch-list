4d94ecbcaee63df9783f5f1d306b89e2b7e51ee7
Allan Sechrist <sechristallan@gmail.com>
2025-01-11 13:41:15 +0900

    Add views

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   before_destroy :check_bookmarks
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, uniqueness: true, presence: true
   validates :overview, presence: true, uniqueness: true
   validates :poster_url, presence: true
   validates :rating, presence: true
